1c9fefb91b21c306d39a59d214d55c9a0bfbe5ff
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2018-04-05 23:01:20 +0000

    sama og committið á undan

--- dummy.py
+++ dummy.py
@@ -20,19 +20,19 @@ fos = []
 lau = []
 for x in range(0, len(listi2), 5):
     lyklar.append(listi2[x])
-    dagar = [listi2[x], listi2[x + 1], listi2[x + 2], listi2[x + 3], listi2[x + 4]]
+    dicta = {listi2[x]: [listi2[x + 1], listi2[x + 2], listi2[x + 3], listi2[x + 4]]}
     if listi2[x + 2] == "1":
-        man.append(dagar)
+        man.append(dicta)
     elif listi2[x + 2] == "2":
-        tri.append(dagar)
+        tri.append(dicta)
     elif listi2[x + 2] == "3":
-        mid.append(dagar)
+        mid.append(dicta)
     elif listi2[x + 2] == "4":
-        fim.append(dagar)
+        fim.append(dicta)
     elif listi2[x + 2] == "5":
-        fos.append(dagar)
+        fos.append(dicta)
     elif listi2[x + 2] == "6":
-        lau.append(dagar)
+        lau.append(dicta)
     # print(listi2[x], end=" ")
 
 man2 = []
@@ -42,12 +42,8 @@ fim2 = []
 fos2 = []
 lau2 = []
 temp = []
-for x in range(len(man)):
-    print(man[x][0], man[x][1], man[x][2], man[x][3], man[x][4])
-    if man[x][0] == man[x + 1][0]:
-        for i in range(len(man)):
-            if man[x][0] == man[x - 1][0]:
-                temp = [man[x][0]]
 
-    else:
-        dummy = "dummy"
+
+
+
+
